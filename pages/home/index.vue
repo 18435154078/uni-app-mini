2bd92abb2bc87081a35d7ab3c781f6a1065335cf
songchao <1064883707@qq.com>
2022-05-23 19:16:45 +0800

    init

---
+++ pages/home/index.vue
@@ -0,0 +1,19 @@
+<template>
+	<view class="content">
+		首页
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
